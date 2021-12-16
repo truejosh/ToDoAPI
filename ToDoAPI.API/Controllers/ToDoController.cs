af746ac98a52a455b9a915f34a717787e615d89d
truejosh <josh9898@yahoo.com>
2021-12-16 16:04:42 -0600

    Add project files.

---
+++ ToDoAPI.API/Controllers/ToDoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.DATA.EF;
+using ToDoAPI.API.Models;
+using System.Web.Http.Cors;
+
+
+namespace ToDoAPI.API.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ToDoController : ApiController
+    {
+        ToDoEntities db = new ToDoEntities();
+
+        public IHttpActionResult GetToDoItems()
+        {
+            List<ToDoViewModel> Items = db.TodoItems.Include("Category").Select(i => new ToDoViewModel()
+            {
+                ID = i.ID,
+                Action = i.Action,
+                Done = i.Done,
+                CategoryId = i.CategoryId,
+                Category = new CategoryViewModel()
+                {
+                    ID = i.Category.ID,
+                    Name = i.Category.Name,
+                    Description = i.Category.Description
+                }
+            }).ToList<ToDoViewModel>();
+
+            if (Items.Count == 0)
+            {
+                return NotFound();//404 error
+            }
+            return Ok(Items);
+        }
+    }
+}
---
+++ ToDoAPI.API/Models/ToDoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoAPI.API.Models
+{
+    public class ToDoViewModel
+    {
+        public int ID { get; set; }
+        public string Action { get; set; }
+        public bool Done { get; set; }
+        public int? CategoryId { get; set; }
+        public virtual CategoryViewModel Category { get; set; }
+    }
+
+    public class CategoryViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+
+}
